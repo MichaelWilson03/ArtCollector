b0e0ba1c40221d8e3ce4b0337e3ebe61327babed
Michael Wilson <michaelbwilson2012@gmail.com>
2023-04-05 23:25:50 -0500

    idk what I'm doing

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,45 +1,71 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
 // Don't touch this import
-import { fetchQueryResultsFromTermAndValue } from '../api';
+import { fetchQueryResultsFromTermAndValue } from "../api";
 
 /**
  * We need a new component called Searchable which:
- * 
+ *
  * Has a template like this:
- * 
+ *
  * <span className="content">
  *  <a href="#" onClick={async (event) => {}}>SOME SEARCH TERM</a>
  * </span>
  *
  * You'll need to read searchTerm, searchValue, setIsLoading, and setSearchResults off of the props.
- * 
+ *
  * When someone clicks the anchor tag, you should:
- * 
+ *
  * - preventDefault on the event
  * - call setIsLoading, set it to true
- * 
+ *
  * Then start a try/catch/finally block:
- * 
+ *
  * try:
  *  - await the result of fetchQueryResultsFromTermAndValue, passing in searchTerm and searchValue
  *  - send the result to setSearchResults (which will update the Preview component)
- * catch: 
+ * catch:
  *  - console.error the error
  * finally:
  *  - call setIsLoading, set it to false
  */
+
 const Searchable = (props) => {
-  
-}
+  const { searchTerm, searchValue, setIsLoading, setSearchResults } = props;
+
+  const handleClick = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const searchResults = await fetchQueryResultsFromTermAndValue(
+        searchTerm,
+        searchValue
+      );
+      setSearchResults(searchResults);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <span className="content">
+      <a href="#" onClick={handleClick}>
+        SOME SEARCH TERM
+      </a>
+    </span>
+  );
+};
 
 /**
  * We need a new component called Feature which looks like this when no featuredResult is passed in as a prop:
- * 
+ *
  * <main id="feature"></main>
- * 
+ *
  * And like this when one is:
- * 
+ *
  * <main id="feature">
  *   <div className="object-feature">
  *     <header>
@@ -57,18 +83,76 @@ const Searchable = (props) => {
  *     </section>
  *   </div>
  * </main>
- * 
- * The different facts look like this: title, dated, images, primaryimageurl, description, culture, style, 
+ *
+ * The different facts look like this: title, dated, images, primaryimageurl, description, culture, style,
  * technique, medium, dimensions, people, department, division, contact, creditline
- * 
+ *
  * The <Searchable /> ones are: culture, technique, medium (first toLowerCase it), and person.displayname (one for each PEOPLE)
- * 
+ *
  * NOTE: people and images are likely to be arrays, and will need to be mapped over if they exist
- * 
+ *
  * This component should be exported as default.
  */
 const Feature = (props) => {
+  if (!props.featuredResult) {
+    return <main id="feature"></main>;
+  }
+  const {
+    title,
+    dated,
+    images,
+    primaryimageurl,
+    description,
+    culture,
+    style,
+    technique,
+    medium,
+    dimensions,
+    people,
+    department,
+    division,
+    contact,
+    creditline,
+  } = props.featuredResult;
 
-}
+  const searchableProps = ["culture", "technique", "medium"];
+
+  return (
+    <main id="feature">
+      <div className="object-feature">
+        <header>
+          <h3>{title}</h3>
+          <h4>{dated}</h4>
+        </header>
+        <section className="facts">
+          {Object.entries(props.featuredResult)
+            .filter(
+              ([key]) =>
+                searchableProps.includes(key) || key.startsWith("people")
+            )
+            .map(([key, value]) => (
+              <React.Fragment key={key}>
+                <span className="title">
+                  {key === "medium"
+                    ? key.charAt(0).toUpperCase() + key.slice(1)
+                    : key}
+                </span>
+                <span className="content">
+                  {Array.isArray(value)
+                    ? value
+                        .map((v) => (v.displayname ? v.displayname : v))
+                        .join(", ")
+                    : value}
+                </span>
+              </React.Fragment>
+            ))}
+        </section>
+        <section className="photos">
+          <img src={primaryimageurl} alt={description} />
+        </section>
+      </div>
+    </main>
+  );
+};
 
 export default Feature;
